60936b98356a6391ba12a533ef4f6d29e60f82b2
Pierce Steward <pierce.steward@gmail.com>
2020-07-21 09:34:24 -0600

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2{
+  public static void main(String[] args) {
+    String name = "Pierce Steward";
+    int age = 31;
+    String state = "Texas";
+    System.out.println ("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
