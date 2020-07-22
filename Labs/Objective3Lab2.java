c78cbb86860598f9b2e537db2204885b8087d19d
Pierce Steward <pierce.steward@gmail.com>
2020-07-22 09:53:02 -0600

    complete Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,8 @@
+public class Objective3Lab2{
+  public static void main(String [] args){
+    double side1=10;
+    double side2=8;
+    double side3=Math.sqrt((side1*side1)+(side2*side2));
+  System.out.println("The hypotenuse of a triangle with sides "+side1+(" and ")+side2+(" is ")+side3);
+  }
+}
