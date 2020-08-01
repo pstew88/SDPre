e0d90356b06ec99be261884f826c576e9a78b687
Pierce Steward <pierce.steward@gmail.com>
2020-07-31 20:39:46 -0600

    complete Objective7Lab5

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+public class Objective7Lab4{
+  public static void main(String[] args) {
+    int count=0;
+    int sum=0;
+
+      while(count <=20) {
+        sum=(sum+count);
+
+        if(count>=20)
+        System.out.println(sum);
+        count=count+1;
+    }
+  }
+}
