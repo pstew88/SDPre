0d6860e2116587698ac911ca49b05667db909d4e
Pierce Steward <pierce.steward@gmail.com>
2020-08-01 22:05:51 -0600

    complete Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,14 @@
+public class Objective8Lab1 {
+  public static void main(String [] args) {
+
+      for (int i=1; i<11; i+=1){
+      System.out.println(i);
+        if (i==10) break;
+        }
+
+      for (int i2=10; i2>0; i2-=1){
+      System.out.println(i2);
+        if (i2==1) break;
+        }
+  }
+}
